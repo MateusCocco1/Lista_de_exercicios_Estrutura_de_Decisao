958ddaa81dc46bcf3259db44c1cd58be4f6f5177
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-15 13:57:46 -0300

    Ex16.py

---
+++ Ex16.py
@@ -0,0 +1,20 @@
+import math
+
+a=float(input("a: "))
+if a==0:
+    print("Não é equação de 2º grau")
+else:
+    b=float(input("b: "))
+    c=float(input("c: "))
+
+    delta=b**2-4*a*c
+
+    if delta<0:
+        print("Não possui raízes reais")
+    elif delta==0:
+        x=-b/(2*a)
+        print("Uma raiz:",x)
+    else:
+        x1=(-b+math.sqrt(delta))/(2*a)
+        x2=(-b-math.sqrt(delta))/(2*a)
+        print("Raízes:",x1,x2)
