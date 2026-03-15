572a3b2c58d9c44a95b199aaaa94337ecad50cd3
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-15 13:53:25 -0300

    Ex08.py

---
+++ Ex08.py
@@ -0,0 +1,7 @@
+p1 = float(input("Preço produto 1: "))
+p2 = float(input("Preço produto 2: "))
+p3 = float(input("Preço produto 3: "))
+
+menor = min(p1,p2,p3)
+
+print("Compre o produto que custa:", menor)
