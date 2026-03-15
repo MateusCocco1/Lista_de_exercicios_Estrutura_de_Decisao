982df9cce0d211a3e348db29441072272206d552
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-15 13:54:32 -0300

    Ex10.py

---
+++ Ex10.py
@@ -0,0 +1,10 @@
+turno = input("Digite M, V ou N: ").upper()
+
+if turno == "M":
+    print("Bom dia!")
+elif turno == "V":
+    print("Boa tarde!")
+elif turno == "N":
+    print("Boa noite!")
+else:
+    print("Valor inválido")
