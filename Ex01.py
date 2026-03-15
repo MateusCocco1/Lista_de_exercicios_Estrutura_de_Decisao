8bc23a66884f9f34a3140ab98aa889e03000cf6f
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-15 13:46:11 -0300

    Ex01.py

---
+++ Ex01.py
@@ -0,0 +1,7 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+if a > b:
+    print("O maior número é:", a)
+else:
+    print("O maior número é:", b)
